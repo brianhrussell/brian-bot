14d6233b72743e625baf26aa5887740c5c5a7000
Brian Russell <brianrussellbr@gmail.com>
2021-01-14 03:02:43 -0600

    supress pylint on import *

--- game_bot.py
+++ game_bot.py
@@ -8,7 +8,7 @@ import game_tracker
 
 from apscheduler.schedulers.asyncio import AsyncIOScheduler
 from events.base_event import BaseEvent
-from events import *
+from events import *  # pylint: disable=unused-wildcard-import
 from multiprocessing import Process
 
 # Set to remember if the bot is already running, since on_ready may be called
