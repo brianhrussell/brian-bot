ab0d698be43eac8cd2dc1adccdc592dce0f18b22
Brian Russell <brianrussellbr@gmail.com>
2021-01-26 20:55:32 -0600

    handle basic state transition from adding players to play

--- games/joinable_game.py
+++ games/joinable_game.py
@@ -1,16 +1,22 @@
 from games.base_game import BaseGame
 from games.base_game import GameCommand
+from enum import Enum
 
 
 class JoinableGame(BaseGame):
 
+    class JoinableGameState(Enum):
+        SETUP = 0
+
     # TODO: uhhh this probably regenerates this list every time? performance check this later
+
     def available_commands(self):
         yield JoinableGame.join_command
 
     def __init__(self, guild, user):
         super().__init__(guild, user)
         self.players = dict()
+        self.state = self.JoinableGameState.SETUP  # to be overwritten but subclasses with actual state enums
 
     # TODO this should probably be higher in the class hierarchy
     def handle_message(self, params, message, client):
@@ -28,6 +34,9 @@ class JoinableGame(BaseGame):
         print('joinable game ended')
 
     def join_game(self, params, message, client):
+        # make a separate function on the game that returns whether a player can be added rather than relying on state
+        if self.state.value != 0:
+            return "game has already started, we can't add you"
         player_id = message.author.id
         player = message.author
         if player_id in self.players:
@@ -36,4 +45,4 @@ class JoinableGame(BaseGame):
         self.players[player_id] = player
         return(f'added {player} to the game.')
 
-    join_command = GameCommand('join', 'join a game in progress', join_game)
+    join_command = GameCommand('join', join_game, 'join a game in progress')
--- games/two_rooms.py
+++ games/two_rooms.py
@@ -1,4 +1,7 @@
 from games.joinable_game import JoinableGame
+from games.joinable_game import GameCommand
+
+from enum import Enum
 
 """
 ok so this is where all the game rules start
@@ -6,7 +9,7 @@ we may need to start breaking out more classes because
 it will probably be too complex for just command handlers.
 
 My idea here is to have a json file with all the possible cards and a class
-that parses that json and knows how each of the cards interact?
+that parses that json and knows how ea  ch of the cards interact?
     So this class will just handle which state of the game are we in
     and possibly the room leadership?
     
@@ -21,11 +24,34 @@ Then, possible
 
 
 class TwoRooms(JoinableGame):
+
+    class TwoRoomsState(Enum):
+        SETUP = 0
+        ON_BEGIN = 1
+
     def __init__(self, guild, user):
         super().__init__(guild, user)
         self.players = dict()
+        self.state = TwoRooms.TwoRoomsState.SETUP
 
     # TODO this is so sus just make the available_commands() for each game static right?
     def available_commands(self):
         for command in super().available_commands():
             yield command
+        yield TwoRooms.begin_game
+        yield GameCommand('begin', TwoRooms.begin_game, 'once all players and roles have been added begin the game and assign roles')
+
+    def selected_roles_are_valid(self):
+        raise NotImplementedError
+        # return true
+
+    def assign_roles(self):
+        raise NotImplementedError
+
+    def begin_game(self, params, message, client):
+        player_id = message.author.id
+        if not self.selected_roles_are_valid():
+            return "the selected roles are not valid sorry," + \
+                " you'll have to figure out why on your own. selected roles: TODO print selected roles"
+        if player_id == self.leader.id:
+            self.assign_roles()
