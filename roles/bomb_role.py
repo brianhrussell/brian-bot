0635626e0902b02a3c942c44e1d32a89aaa63997
Brian Russell <brianrussellbr@gmail.com>
2021-01-26 21:43:15 -0600

    move roles to correct folder add properties

---
+++ roles/bomb_role.py
@@ -0,0 +1,15 @@
+from role_utils import BaseRole
+from role_utils import CardColor
+from role_utils import TeamColor
+
+
+class BombRole(BaseRole):
+    def __init__(self):
+        self.name = "Bomb"
+        self.directions = "TODO."
+        self.color = CardColor.RED
+        self.team = TeamColor.RED
+        self.max_role_count = 1
+        self.allows_buried_role = False
+        self.modifiers = list()
+        self.required_roles = ['President']
---
+++ roles/president_role.py
@@ -0,0 +1,15 @@
+from role_utils import BaseRole
+from role_utils import CardColor
+from role_utils import TeamColor
+
+
+class PresidentRole(BaseRole):
+    def __init__(self):
+        self.name = "President"
+        self.directions = "TODO."
+        self.color = CardColor.BLUE
+        self.team = TeamColor.BLUE
+        self.max_role_count = 1
+        self.allows_buried_role = False
+        self.modifiers = list()
+        self.required_roles = ['Bomb']
