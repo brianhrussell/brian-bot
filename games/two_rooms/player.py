8f739dc2b8d6a5042cd82260c66c2f4b4844ad28
Brian Russell <brianrussellbr@gmail.com>
2021-01-30 18:24:11 -0600

    add get_dm_channel to player

--- games/two_rooms/player.py
+++ games/two_rooms/player.py
@@ -2,3 +2,8 @@ class Player:
     def __init__(self, discord_user, role):
         self.user = discord_user
         self.role = role
+
+    async def get_dm_channel(self):
+        if self.user.dm_channel:
+            return self.user.dm_channel
+        return await self.user.create_dm()
